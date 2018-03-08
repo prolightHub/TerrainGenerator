891a195e7c31c44fb8147862b29e391dc2b697e8
prolightHub <36085162+prolightHub@users.noreply.github.com>
2018-03-07 16:56:44 -0800

    Add files via upload

---
+++ template/template.js
@@ -0,0 +1,35 @@
+var canvas = document.getElementById("canvas");
+var processing = new Processing(canvas, function(processing) {
+    processing.size(400, 400);
+    processing.background(0xFFF);
+
+    var mouseIsPressed = false;
+    processing.mousePressed = function () { mouseIsPressed = true; };
+    processing.mouseReleased = function () { mouseIsPressed = false; };
+
+    var keyIsPressed = false;
+    processing.keyPressed = function () { keyIsPressed = true; };
+    processing.keyReleased = function () { keyIsPressed = false; };
+
+    function getImage(s) {
+        var url = "https://www.kasandbox.org/programming-images/" + s + ".png";
+        processing.externals.sketch.imageCache.add(url);
+        return processing.loadImage(url);
+    }
+
+    function getLocalImage(url) {
+        processing.externals.sketch.imageCache.add(url);
+        return processing.loadImage(url);
+    }
+
+    // use degrees rather than radians in rotate function
+    var rotateFn = processing.rotate;
+    processing.rotate = function (angle) {
+        rotateFn(processing.radians(angle));
+    };
+
+    with (processing) {
+      
+    }
+    if (typeof draw !== 'undefined') processing.draw = draw;
+});
